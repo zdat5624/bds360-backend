43cb46496c08cce073ddf3f00cb301b7cca40223
zdat5624 <zdat5624@gmail.com>
2026-09-04 16:17:43 +0700

    first commit

---
+++ listing-service/src/main/java/com/example/listing_service/ListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.listing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingServiceApplication.class, args);
+	}
+
+}
